753e816aada0425d352e98d7f00511c2598c839f
Cristela89 <cristelamarayala@gmail.com>
2025-02-26 14:13:24 -0400

    portafolio3

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   portafolioBtn.addEventListener('click', function() {
-    scrollAndCloseMenu(7080); // Ir a la sección Portafolio
+    scrollAndCloseMenu(6600); // Ir a la sección Portafolio
   });
 
   contactoBtn.addEventListener('click', function() {
