3e9e4b4451a1a2a3d914ec3450a1e2273801f89f
Cristela89 <cristelamarayala@gmail.com>
2025-04-21 10:43:10 -0400

    scroll3

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   misRendersBtn.addEventListener('click', function() {
-    scrollAndCloseMenu(8955); // Ir a la sección Portafolio
+    scrollAndCloseMenu(8655); // Ir a la sección Portafolio
   });
 
   contactoBtn.addEventListener('click', function() {
