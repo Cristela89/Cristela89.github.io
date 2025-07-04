67acea0602d6c95a2ec277aead06d1a49f4c2f34
Cristela-0109 <cristela.marazul@gmail.com>
2025-07-04 08:31:30 -0400

    ver

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+    document.getElementById('home-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+
+    document.getElementById('sobre-mi-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 671,
+        behavior: 'smooth'
+      });
+    });
+
+    document.getElementById('educacion-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 1940,
+        behavior: 'smooth'
+      });
+    });
+
+    document.getElementById('experiencia-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 2547,
+        behavior: 'smooth'
+      });
+    });
+
+    document.getElementById('portafolio-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 3627,
+        behavior: 'smooth'
+      });
+    });
+
+    document.getElementById('contacto-btn').addEventListener('click', function () {
+      window.scrollTo({
+        top: 13570,
+        behavior: 'smooth'
+      });
+    });
